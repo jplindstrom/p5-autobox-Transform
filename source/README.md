e1ed6e614778ee23f7a1a1dc820dca941b848ea6
Johan Lindstrom <jpl@nospam>
2016-02-25 22:42:28 +0000

    Initial commit

---
+++ source/README.md
@@ -0,0 +1,22 @@
+# NAME
+
+autobox::Transform - It's new $module
+
+# SYNOPSIS
+
+    use autobox::Transform;
+
+# DESCRIPTION
+
+autobox::Transform is ...
+
+# LICENSE
+
+Copyright (C) Johan Lindstrom.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Johan Lindstrom &lt;jpl@nospam>
---
+++ source/lib/autobox/Transform.pm
@@ -0,0 +1,39 @@
+package autobox::Transform;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+autobox::Transform - It's new $module
+
+=head1 SYNOPSIS
+
+    use autobox::Transform;
+
+=head1 DESCRIPTION
+
+autobox::Transform is ...
+
+=head1 LICENSE
+
+Copyright (C) Johan Lindstrom.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Johan Lindstrom E<lt>jpl@nospamE<gt>
+
+=cut
+
